3fcc003fdbd2dd7473036c7ff1977b3a654289c8
v1lling <sascha.villing@web.de>
2024-11-15 16:53:35 +0100

    Fix build

--- cloudapp/src/app/models/UserInformation.ts
+++ cloudapp/src/app/models/UserInformation.ts
@@ -1,6 +1,7 @@
 export class UserInformation {
     primary_id: string;
     email: string;
+    note: string;
 
     constructor(init?: Partial<UserInformation>) {
         Object.assign(this, init);
